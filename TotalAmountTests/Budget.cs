cebe67275b5b2f42e022dd41a7c262aed29bb52e
hatelove <jay.hatelove@gmail.com>
2017-09-15 00:53:30 +0800

    rename

--- TotalAmountTests/Budget.cs
+++ TotalAmountTests/Budget.cs
@@ -29,10 +29,10 @@ namespace TotalAmountTests
         public double GetOverlappingAmount(Period period)
         {
             var periodOfBudget = new Period(this.GetStartDate(), this.GetEndDate());
-            var daysOfPeriod = period.GetOverlappingDays(periodOfBudget);
+            var overlappingDays = period.GetOverlappingDays(periodOfBudget);
             var dailyAmount = this.GetDailyAmount();
 
-            return daysOfPeriod * dailyAmount;
+            return overlappingDays * dailyAmount;
         }
     }
 }

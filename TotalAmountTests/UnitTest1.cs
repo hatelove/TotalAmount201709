8bee8eb55d571aa1ee023c74876e086e7470f78f
hatelove <jay.hatelove@gmail.com>
2017-09-14 16:01:17 +0800

    first failed test case: no planned budget

---
+++ TotalAmountTests/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace TotalAmountTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void no_planned_budgets_AmountShouldBe_0()
+        {
+            var stubBudgetRepo = Substitute.For<IBudgetRepo>();
+            stubBudgetRepo.FindAll().Returns(new List<Budget>());
+
+            var totalAmount = new TotalAmount(stubBudgetRepo);
+
+            var actual = totalAmount.query("20170620", "20170715");
+
+            Assert.AreEqual(150, actual);
+        }
+    }
+
+    public class Budget
+    {
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> FindAll();
+    }
+
+    public class TotalAmount
+    {
+        private IBudgetRepo stubBudgetRepo;
+
+        public TotalAmount(IBudgetRepo stubBudgetRepo)
+        {
+            this.stubBudgetRepo = stubBudgetRepo;
+        }
+
+        public double query(string startDate, string endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

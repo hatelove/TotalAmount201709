00c7cb3905e3d3a73c5f77d91db2ab19f79aeb96
hatelove <jay.hatelove@gmail.com>
2017-09-15 01:05:33 +0800

    refactor

--- TotalAmountTests/Budget.cs
+++ TotalAmountTests/Budget.cs
@@ -4,27 +4,8 @@ namespace TotalAmountTests
 {
     public class Budget
     {
-        public string Month { get; set; }
         public double Amount { get; set; }
-
-        public DateTime GetStartDate()
-        {
-            var dateInString = this.Month + "01";
-            return DateTime.ParseExact(dateInString, "yyyyMMdd", null);
-        }
-
-        public DateTime GetEndDate()
-        {
-            var startDate = GetStartDate();
-            var endDay = DateTime.DaysInMonth(startDate.Year, startDate.Month);
-            return new DateTime(startDate.Year, startDate.Month, endDay);
-        }
-
-        public double GetDailyAmount()
-        {
-            var dailyAmount = this.Amount / this.GetEndDate().Day;
-            return dailyAmount;
-        }
+        public string Month { get; set; }
 
         public double GetOverlappingAmount(Period period)
         {
@@ -33,6 +14,25 @@ namespace TotalAmountTests
             var dailyAmount = this.GetDailyAmount();
 
             return overlappingDays * dailyAmount;
+        }
+
+        private double GetDailyAmount()
+        {
+            var dailyAmount = this.Amount / this.GetEndDate().Day;
+            return dailyAmount;
+        }
+
+        private DateTime GetEndDate()
+        {
+            var startDate = GetStartDate();
+            var endDay = DateTime.DaysInMonth(startDate.Year, startDate.Month);
+            return new DateTime(startDate.Year, startDate.Month, endDay);
+        }
+
+        private DateTime GetStartDate()
+        {
+            var dateInString = this.Month + "01";
+            return DateTime.ParseExact(dateInString, "yyyyMMdd", null);
         }
     }
 }
--- TotalAmountTests/TotalAmount.cs
+++ TotalAmountTests/TotalAmount.cs
@@ -3,25 +3,6 @@ using System.Linq;
 
 namespace TotalAmountTests
 {
-    public class Period
-    {
-        public Period(DateTime start, DateTime end)
-        {
-            Start = start;
-            End = end;
-        }
-
-        public DateTime Start { get; private set; }
-        public DateTime End { get; private set; }
-
-        public int GetOverlappingDays(Period another)
-        {
-            var overlapStart = this.Start > another.Start ? this.Start : another.Start;
-            var overlapEnd = this.End < another.End ? this.End : another.End;
-            return (overlapEnd.AddDays(1) - overlapStart).Days;
-        }
-    }
-
     public class TotalAmount
     {
         private IBudgetRepo budgetRepo;
